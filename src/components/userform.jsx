1108e9f4542c1d6501c699207cddc60e63b7259a
Amani5 <amanibouaziz55@gmail.com>
2021-09-08 11:23:09 +0200

    update pass params

--- src/components/userform.jsx
+++ src/components/userform.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
-import { withRouter } from 'react-router-dom'; 
+import { withRouter } from "react-router-dom";
 
 export function UserForm(props) {
-    const [name, setName] = useState("");
-    const [lastname, setLastName] = useState("");
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        props.history.push('/characters');
-    }
+  const [name, setName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.history.push({
+      pathname: "/characters",
+      state: {name , lastname}
+    });
+  };
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text"
-        value={name}
-        onChange={e => setName(e.target.value)} />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </label>
       <label>
         Last Name:
-        <input type="text" 
-        value={lastname}
-        onChange={e => setLastName(e.target.value)}/>
+        <input
+          type="text"
+          value={lastname}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </label>
       <input type="submit" value="Submit" />
     </form>
